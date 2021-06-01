6f97fd163a72da82c9637fe2a084dd35e88e165c
mizore <tangxinbos@gmail.com>
2021-06-01 15:29:02 +0900

    二回目のプロジェクトコミット

--- Test01/src/Demo.java
+++ Test01/src/Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
-    public static void main(String[] args) {
-        Systme.out.println("初めましてのIDEA");
-        Systme.out.println("プロジェクト管理者がソースを更新しました。");
+        public static void main(String[] args) {
+            System.out.println("初めましてのIDEA");
+            System.out.println("プロジェクト管理者がソースを更新しました。");
+        }
     }
-}
